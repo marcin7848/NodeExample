e2f8fc2264e45298fa01286fc0b86319d1dfdf0a
marcin7848 <marcin7848@gmail.com>
2019-12-05 16:18:01 +0100

    connection

--- app.js
+++ app.js
@@ -10,40 +10,20 @@ const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = req
 const port = 8080;
 
 
-
-var db_config = {
+var mysql = require('mysql');
+var pool  = mysql.createPool({
     host: '192.168.99.100',
     user: 'root',
     password: 'root',
     database: 'socka'
-};
+});
 
-var connection;
+pool.getConnection(function(err, connection) {
+  // connected! (unless `err` is set)
+  connection.end();
+});
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
-
-global.db = connection;
+global.db = pool;
 
 // configure middleware
 app.set('port', process.env.port || port); // set express to use this port
