39146d209eea8cf830ef405b42d44592bf60888e
marcin7848 <marcin7848@gmail.com>
2019-12-05 15:09:53 +0100

    connection repaired

--- app.js
+++ app.js
@@ -9,23 +9,41 @@ const {getHomePage} = require('./routes/index');
 const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
 const port = 8080;
 
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection ({
+
+
+var db_config = {
     host: '192.168.99.100',
     user: 'user',
     password: 'user',
     database: 'socka'
-});
+};
 
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-    console.log('Connected to database');
-});
-global.db = db;
+  });
+}
+
+handleDisconnect();
+
+global.db = connection;
 
 // configure middleware
 app.set('port', process.env.port || port); // set express to use this port
