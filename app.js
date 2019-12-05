9f466f881da1a72482f8170d1779fec7dfd904b0
marcin7848 <marcin7848@gmail.com>
2019-12-05 12:57:36 +0100

    ip changed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 2000;
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host: '55.55.55.5',
+    host: '192.168.99.100',
     user: 'user',
     password: 'user',
     database: 'socka'
